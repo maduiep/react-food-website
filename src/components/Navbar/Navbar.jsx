ec7d9de4d1910763b64f4de92ac5b1c96636e9d0
Emmanuel Madu IP <emmanuelmaduip@gmail.com>
2021-09-15 16:03:27 +0100

    initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Nav, NavLink} from './NavbarElement'
+
+function Navbar() {
+    return (
+        <>
+           <Nav>
+               <NavLink to='/'>Pizza</NavLink>
+           </Nav>
+        </>
+    )
+}
+
+export default Navbar
